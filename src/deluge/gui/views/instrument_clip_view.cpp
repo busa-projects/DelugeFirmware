ce61b92972d5388d1d12f3988c62cbae28b20a8c
busa-projects <busa@gmx.ch>
2026-01-11 11:58:40 +0100

    Adding FILL Popup and Audition-Pad

--- src/deluge/gui/views/instrument_clip_view.cpp
+++ src/deluge/gui/views/instrument_clip_view.cpp
@@ -3407,6 +3407,9 @@ multiplePresses:
 		else if (changeType == CORRESPONDING_NOTES_SET_ITERANCE) {
 			displayIterance(Iterance::fromInt(parameterValue));
 		}
+		else if (changeType == CORRESPONDING_NOTES_SET_FILL) {
+			displayFill(parameterValue);
+		}
 	}
 }
 
@@ -3467,14 +3470,27 @@ void InstrumentClipView::displayIterance(Iterance iterance) {
 	}
 }
 
-const char* InstrumentClipView::getFillString(uint8_t fill) {
+void InstrumentClipView::displayFill(uint8_t mode) {
+	char buffer[(display->haveOLED()) ? 29 : 5];
+
+	strcpy(buffer, getFillString(mode));
+
+	if (display->haveOLED()) {
+		display->popupText(buffer, PopupType::FILL);
+	}
+	else {
+		display->displayPopup(buffer, 0, true, 255, 1, PopupType::FILL);
+	}
+}
+
+const char* InstrumentClipView::getFillString(uint8_t mode) {
 	// FILL mode
-	if (fill == FILL) {
+	if (mode == FILL) {
 		return "FILL";
 	}
 
 	// NO-FILL mode
-	if (fill == NOT_FILL) {
+	if (mode == NOT_FILL) {
 		return "NOT FILL";
 	}
 
@@ -3922,7 +3938,8 @@ int32_t InstrumentClipView::setNoteRowParameterValue(int32_t withOffset, int32_t
 		return -1; // Get out if NoteRow doesn't exist and can't be created
 	}
 
-	bool hasPopup = display->hasPopupOfType(PopupType::PROBABILITY) || display->hasPopupOfType(PopupType::ITERANCE);
+	bool hasPopup = display->hasPopupOfType(PopupType::PROBABILITY) || display->hasPopupOfType(PopupType::ITERANCE)
+	                || display->hasPopupOfType(PopupType::FILL);
 
 	uint16_t originalParameter;
 	bool parameterHasBeenEdited = false;
@@ -3989,6 +4006,11 @@ int32_t InstrumentClipView::setNoteRowParameterValue(int32_t withOffset, int32_t
 						parameterValue++;
 						parameterHasBeenEdited = true;
 					}
+				}
+				// Wrap around for FILL when at max value
+				else if (parameterValue == parameterMaxValue && changeType == CORRESPONDING_NOTES_SET_FILL) {
+					parameterValue = parameterMinValue;
+					parameterHasBeenEdited = true;
 				}
 			}
 			// Decrementing
@@ -4066,6 +4088,9 @@ int32_t InstrumentClipView::setNoteRowParameterValue(int32_t withOffset, int32_t
 		}
 		else if (changeType == CORRESPONDING_NOTES_SET_ITERANCE) {
 			displayIterance(Iterance::fromInt(parameterValue));
+		}
+		else if (changeType == CORRESPONDING_NOTES_SET_FILL) {
+			displayFill(parameterValue);
 		}
 	}
 
